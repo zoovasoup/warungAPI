c6ce06968bc3b424f9a7404989f576f3ca92d19f
zoovasoup <zulfa.plank@proton.me>
2025-05-25 19:31:41 +0700

    git init

---
+++ src/main/java/Tubet_DPBO/Warung_API/WarungApiApplication.java
@@ -0,0 +1,13 @@
+package Tubet_DPBO.Warung_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarungApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarungApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tubet_DPBO/Warung_API/WarungApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Tubet_DPBO.Warung_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarungApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
