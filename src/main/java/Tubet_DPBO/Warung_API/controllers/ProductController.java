ca178dc3ef2a80ce378b63b07cce8d7f2bdf9068
zoovasoup <zulfa.plank@proton.me>
2025-05-28 03:22:51 +0700

    [feat] product crud

--- src/main/java/Tubet_DPBO/Warung_API/controllers/ProductController.java
+++ src/main/java/Tubet_DPBO/Warung_API/controllers/ProductController.java
@@ -2,7 +2,13 @@ package Tubet_DPBO.Warung_API.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +27,35 @@ public class ProductController {
 		this.productRepo = productRepo;
 	}
 
+	@PostMapping
+	public ResponseEntity<Product> createProduct(Product product) {
+		Product created = productRepo.put(product);
+		return new ResponseEntity<>(created, HttpStatus.CREATED);
+	}
+
 	@GetMapping("/all")
 	public List<Product> getAllProduct() {
 		return productRepo.getAll();
+	}
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<Product> updateProduct(@PathVariable String id, Product product) {
+		Product updated = productRepo.patch(id, product);
+		if (updated != null) {
+			return new ResponseEntity<>(updated, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable String id) {
+		boolean product = productRepo.delete(id);
+		if (productRepo.delete(id)) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
